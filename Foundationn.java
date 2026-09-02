2c6509fce044c47e35d949c8f3807135a10edd11
afiffataj <afiffataj@gmail.com>
2026-09-03 00:26:15 +0530

    instance variables

---
+++ Foundationn.java
@@ -0,0 +1,19 @@
+class Foundationn{
+	
+	int shade=3;
+	String brand="lakeme";
+	double price=1500;
+	int quantity=3;
+	String type="liquid";
+	double coverage;
+	int expiryMonths;
+	String color;
+	double weight;
+	int stock;
+	
+	public Foundationn()
+	{
+		System.out.println("no-args is in the constructor");
+	}
+	
+}
