faba5da84e3a1d159269974cd30094d63c95016b
afiffataj <afiffataj@gmail.com>
2026-09-02 22:03:22 +0530

    instance variables

---
+++ Injection.java
@@ -0,0 +1,18 @@
+class Injection{
+	
+	int dosage=10;
+	String medicine="antibiotic";
+	double price=250.54;
+	int quantity=5;
+	String brand="cipla";
+	double volume;
+	int age;
+	String manufacture;
+	double weight;
+	int stock;
+	
+	public Injection()
+	{
+		System.out.println("Injection is the non-primitive");
+	}
+}
