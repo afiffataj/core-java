f522be42be83a828c3c301f550e9ea7405ab7f63
afiffataj <afiffataj@gmail.com>
2026-09-10 21:55:31 +0530

    variables

---
+++ Shoess.java
@@ -0,0 +1,18 @@
+class Shoess{
+	
+	public static void main(String[] args){
+		System.out.println("shoess are for sports");
+		
+		String brand="Nike";
+		int price=5000;
+		String color="White";
+		int size=30;
+		double warrenty=3.5;
+
+        System.out.println("brand="+brand);
+		System.out.println("price="+price);
+		System.out.println("color="+color);
+		System.out.println("size="+size);
+		System.out.println("warrenty="+warrenty);
+	}
+}
