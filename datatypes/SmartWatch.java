f1d211cc3971062893f04c2feccbafab9ae1db02
afiffataj <afiffataj@gmail.com>
2026-09-12 00:50:48 +0530

    variables

---
+++ datatypes/SmartWatch.java
@@ -0,0 +1,25 @@
+class SmartWatch{
+	public static void main(String[] args){
+		System.out.println("SmartWatch are very expensive");
+		
+		String color="Royal Blue";
+		String brand="Boat";
+		int price=3299;
+		byte stars=4;
+		short discount=45;
+		long id=567464546l;
+		float weight=14.6f;
+		double length=35.5d;
+		boolean isBluetooth=true;
+		
+		System.out.println("color="+color);
+		System.out.println("brand="+brand);
+		System.out.println("price="+price);
+		System.out.println("stars="+stars);
+		System.out.println("discount="+discount);
+		System.out.println("id="+id);
+		System.out.println("weight="+weight);
+		System.out.println("length="+length);
+		System.out.println("isBluetooth="+isBluetooth);
+	}
+}
