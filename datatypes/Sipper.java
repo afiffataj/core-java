cd18b2710a8b78849259dcc07d216fd0ba5cae5f
afiffataj <afiffataj@gmail.com>
2026-09-12 00:20:06 +0530

    variables

---
+++ datatypes/Sipper.java
@@ -0,0 +1,29 @@
+class Sipper{
+	public static void main(String[] args){
+		System.out.println("Sipper is water container");
+		
+		String brand="Nestasia";
+		String material="Stainless Steel";
+		String color="Blue";
+		int price=2500;
+		byte length=45;
+		char size='M';
+		short noOfPiece=56;
+		boolean isavailable=true;
+		long customerNumber=9874564738l;
+		float capacity=15.6f;
+		double weight=1000d;
+		
+		System.out.println("brand="+brand);
+		System.out.println("material="+material);
+		System.out.println("color="+color);
+		System.out.println("price="+price);
+		System.out.println("length="+length);
+		System.out.println("size="+size);
+		System.out.println("noOfPiece="+noOfPiece);
+		System.out.println("isavailable="+isavailable);
+		System.out.println("customerNumber="+customerNumber);
+		System.out.println("capacity="+capacity);
+		System.out.println("weight="+weight);
+	}
+}
