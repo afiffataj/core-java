9506b5fb42af1d614e2727e8c828aa55941d5825
afiffataj <afiffataj@gmail.com>
2026-09-12 00:19:14 +0530

    variables

---
+++ datatypes/Bench.java
@@ -0,0 +1,19 @@
+class Bench{
+	public static void main(String[] args){
+		System.out.println("Bench are made for sitting");
+		
+		int price=8000;
+		int length=120;
+		String brand="ikkea";
+		String type="Garden Bench";
+		double weight=45.6;
+		String color="Black";
+		
+		System.out.println("price="+price);
+		System.out.println("length="+length);
+		System.out.println("brand="+brand);
+		System.out.println("type="+type);
+		System.out.println("weight="+weight);
+		System.out.println("color="+color);
+	}
+}
