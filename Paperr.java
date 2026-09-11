3701792a9b0369b41c83c92047271edd2def7f21
afiffataj <afiffataj@gmail.com>
2026-09-11 18:50:03 +0530

    variabes

---
+++ Paperr.java
@@ -0,0 +1,25 @@
+class Paperr{
+	public static void main(String[] args){
+		System.out.println("Paper is used for many purpose");
+		
+		String brand="Classmate";
+		byte noOfPapers=100;
+		int noOfBooks=1000;
+		short noOfBookBundles=1289;
+		long noOfLines=87645678932l;
+		double weight=35.8d;
+		char size='M';
+		float lengthInCm=40f;
+		boolean isavailable=true;
+		
+		System.out.println("brand of paper="+brand);
+		System.out.println("noOfPapers of paper="+noOfPapers);
+		System.out.println("noOfBooks of paper="+noOfBooks);
+		System.out.println("noOfBookBundles of paper="+noOfBookBundles);
+		System.out.println("noOfLines of paper="+noOfLines);
+		System.out.println("weight of paper="+weight);
+		System.out.println("size of paper="+size);
+		System.out.println("lengthInCm of paper="+lengthInCm);
+		System.out.println("isavailable of paper="+isavailable);
+}
+}
