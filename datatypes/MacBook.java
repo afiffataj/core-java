295b7bd132938cd639fabae8c78db60763aa5aea
afiffataj <afiffataj@gmail.com>
2026-09-12 00:51:45 +0530

    variables

---
+++ datatypes/MacBook.java
@@ -0,0 +1,27 @@
+class MacBook{
+	public static void main(String[] args){
+		System.out.println("MacBook are used for high purpose");
+		
+		String brand="BlueSwan";
+		String color="Frosted Clear";
+		String compatibleDevices="2026 MacBook Neo 13 inch A18 Pro";
+		String formFactor="Case";
+		String shellType="Hard";
+		int price=1799;
+		int discount=17;
+		double warrenty=1;
+		
+		System.out.println("brand="+brand);
+		System.out.println("color="+color);
+		System.out.println("compatibleDevices="+compatibleDevices);
+		System.out.println("formFactor="+formFactor);
+		System.out.println("shellType="+shellType);
+		System.out.println("price="+price);
+		System.out.println("discount="+discount);
+		System.out.println("warrenty="+warrenty);
+		
+		
+		
+		
+	}
+}
