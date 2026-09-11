57b365056358fb5118d300f9827ad2b870fac3bb
afiffataj <afiffataj@gmail.com>
2026-09-12 00:55:54 +0530

    variables

---
+++ datatypes/Primerr.java
@@ -0,0 +1,25 @@
+class Primerr{
+	public static void main(String[] args){
+		System.out.println("Primer is appled in the basic");
+		
+		int price=345;
+		float discount=14f;
+		String brand="Renee";
+		String productBenefits="Radiance";
+		String itemForm="Cream";
+		String skinType="all";
+		String country="India";
+		byte noOfItems=1;
+		double netQuantity=30.0d;
+		
+		System.out.println("price of Primerr="+price);
+		System.out.println("discount for Primerr="+discount);
+		System.out.println("brand of Primerr="+brand);
+		System.out.println("productBenefits of Primerr="+productBenefits);
+		System.out.println("itemForm of Primerr="+itemForm);
+		System.out.println("skinType of Primerr="+skinType);
+		System.out.println("country of Primerr="+country);
+        System.out.println("noOfItems of Primerr="+noOfItems);
+        System.out.println("netQuantity of Primerr="+netQuantity);		
+	}
+}
