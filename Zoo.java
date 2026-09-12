b0833701183b793405c3df315461fe580492a276
afiffataj <afiffataj@gmail.com>
2026-09-13 00:38:14 +0530

    variables

---
+++ Zoo.java
@@ -0,0 +1,16 @@
+class Zoo{
+	public static void main(String[] args){
+		
+		int noOfAnimals=900;
+		String zooKeeperName="Surya";
+		int lengthOfZoo=45900;
+		String zooName="Mysuru Zoo";
+		int noOfWildAnimals=489;
+		
+		System.out.println("noOfAnimals="+noOfAnimals);
+		System.out.println("zooKeeperName="+zooKeeperName);
+		System.out.println("lengthOfZoo="+lengthOfZoo);
+		System.out.println("zooName="+zooName);
+		System.out.println("noOfWildAnimals="+noOfWildAnimals);
+	}
+}
