05d1c948a884ab641b4bbc84ac3f5da7ba2404a1
afiffataj <afiffataj@gmail.com>
2026-09-12 11:35:46 +0530

    variables

---
+++ Hospitall.java
@@ -0,0 +1,21 @@
+class Hospitall{
+	public static void main(String[] args){
+		System.out.println("there are many hospitals");
+		
+		String location="Bengaluru";
+		double width=4456.9;
+		int noOfDoctors=50;
+		int noOfPatients=300;
+		double diameter=4509;
+		int noOfFloors=4;
+		int noOfBeds=400;
+		
+		System.out.println("location="+location);
+		System.out.println("width="+width);
+		System.out.println("noOfDoctors="+noOfDoctors);
+		System.out.println("noOfPatients="+noOfPatients);
+		System.out.println("diameter="+diameter);
+		System.out.println("noOfFloors="+noOfFloors);
+		System.out.println("noOfBeds="+noOfBeds);
+	}
+}
