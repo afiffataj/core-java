2671e8e62f784490a46fe84b2ae57f2b8af8daa6
afiffataj <afiffataj@gmail.com>
2026-09-13 00:33:38 +0530

    variables

---
+++ Sunflower.java
@@ -0,0 +1,19 @@
+class Sunflower{
+	public static void main(String[] args){
+		System.out.println("Sunflower are rarely found");
+		
+		int price=500;
+		String color="Yellow";
+		int length=50;
+		double weight=20;
+		int noOfFlower=5;
+		
+		System.out.println("price="+price);
+		System.out.println("color="+color);
+		System.out.println("length="+length);
+		System.out.println("weight="+weight);
+		System.out.println("noOfFlower="+noOfFlower);
+		
+	}
+}
+		
