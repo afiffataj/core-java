ef9595004a364529d4167f4bad321f479617723f
afiffataj <afiffataj@gmail.com>
2026-09-14 02:00:02 +0530

    variables

---
+++ datatypes/Telescope.java
@@ -0,0 +1,25 @@
+class Telescope{
+	public static void main(String[] args){
+		System.out.println("Telescope is an electronic device");
+		
+		String brand="CEZO";
+		String modelName="CZ-T01";
+		byte styleName=70;
+		int opticalTube=300;
+		short lensDiameter=700;		
+		long customerNumber=9880974777l;
+		boolean isavailable=false;
+		float price=3009.7f;
+		double discount=60.4d;
+		
+		System.out.println("brand="+brand);
+		System.out.println("modelName="+modelName);
+		System.out.println("styleName="+styleName);
+		System.out.println("opticalTube="+opticalTube);
+		System.out.println("lensDiameter="+lensDiameter);
+		System.out.println("customerNumber="+customerNumber);
+		System.out.println("isavailable="+isavailable);
+		System.out.println("price="+price);
+		System.out.println("discount="+discount);
+	}
+}
