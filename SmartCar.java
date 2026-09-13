e202c7b09e6a4029166f8b61bf8a82197984c077
afiffataj <afiffataj@gmail.com>
2026-09-13 15:51:07 +0530

    variables

---
+++ SmartCar.java
@@ -0,0 +1,23 @@
+class SmartCar{
+	public static void main(String[] args){
+		System.out.println("SmartCar is used in modern world");
+		
+		String brand="tesla";
+		String model="Model 3";
+		String color="black";
+		int price=400000;
+		int speed=201;
+		double range=500;
+		boolean isElectric=true;
+		
+		System.out.println("brand="+brand);
+		System.out.println("model="+model);
+		System.out.println("color="+color);
+		System.out.println("price="+price);
+		System.out.println("speed="+speed);
+		System.out.println("range="+range);
+		System.out.println("isElectric="+isElectric);
+	}
+}
+
+		
