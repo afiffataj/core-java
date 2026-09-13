f01758f8dcbb70a9b00756e8c240ad0b0b588a51
afiffataj <afiffataj@gmail.com>
2026-09-13 15:03:34 +0530

    variables

---
+++ Monitor.java
@@ -0,0 +1,19 @@
+class Monitor{
+	public static void main(String[] args){
+		System.out.println("Monitor is important");
+		
+		String brand="Dell";
+		String model="monitor";
+		int screenSize=23;
+		int price=25000;
+		boolean hasSpeaker=true;
+		double weight=34.7;
+		
+		System.out.println("brand="+brand);
+		System.out.println("model="+model);
+		System.out.println("screenSize="+screenSize);
+		System.out.println("price="+price);
+		System.out.println("hasSpeaker="+hasSpeaker);
+		System.out.println("weight="+weight);
+	}
+}
