03553b491f216f867f3a5486a48ec3b173dd1df0
afiffataj <afiffataj@gmail.com>
2026-09-14 01:33:16 +0530

    variables

---
+++ Doctor.java
@@ -0,0 +1,19 @@
+class Doctor{
+	public static void main(String[] args){
+		System.out.println("Doctors are human beings");
+		
+		String doctorName="Dr Rahul";
+		int age=35;
+		String hospitalName="Apollo";
+		int experience=23;
+		String city="Mumbai";
+		boolean available=true;
+		
+		System.out.println("doctorName="+doctorName);
+		System.out.println("age="+age);
+		System.out.println("hospitalName="+hospitalName);
+		System.out.println("experience="+experience);
+		System.out.println("city="+city);
+		System.out.println("available="+available);
+	}
+}
