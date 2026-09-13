d248ed7ffd0982085cf5c33c2d06cadd72dbda41
afiffataj <afiffataj@gmail.com>
2026-09-13 14:53:32 +0530

    variables

---
+++ Fann.java
@@ -0,0 +1,18 @@
+class Fann{
+	public static void main(String[] args){
+		System.out.println("Fann is the object");
+		
+		String brand="Usha";
+		String color="White";
+		int price=4500;
+		int speed=56;
+		double length=34;
+		
+		System.out.println("brand="+brand);
+		System.out.println("color="+color);
+		System.out.println("price="+price);
+		System.out.println("speed="+speed);
+		System.out.println("length="+length);
+		
+	}
+}
