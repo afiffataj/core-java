85a1a359d2e21f41a31fddb4af09d9ad5f5d8056
afiffataj <afiffataj@gmail.com>
2026-09-13 14:58:28 +0530

    variables

---
+++ Building.java
@@ -0,0 +1,24 @@
+class Building{
+	public static void main(String[] args){
+		System.out.println("Building is constructed");
+		
+		String name="Green house";
+		String type="apartment";
+		String address="Banglore";
+		int noOfFloors=13;
+		int noOfRooms=45;
+		String ownerName="Raj";
+		double area=245800.9;
+		
+		System.out.println("name="+name);
+		System.out.println("type="+type);
+		System.out.println("address="+address);
+		System.out.println("noOfFloors="+noOfFloors);
+		System.out.println("noOfRooms="+noOfRooms);
+		System.out.println("ownerName="+ownerName);
+		System.out.println("area="+area);
+	}
+}
+		
+		
+		
