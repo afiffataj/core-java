9a407f5ad3e8b75c8be103a036a56d2d18b62fc9
afiffataj <afiffataj@gmail.com>
2026-09-14 01:28:35 +0530

    variables

---
+++ Laptopp.java
@@ -0,0 +1,19 @@
+class Laptopp{
+	public static void main(String[] args){
+		System.out.println("Laptops are used for basic purpose");
+		
+		String color="Silver";
+		int price=80000;
+		String brand="HP";
+		String model="Inspiron 15";
+		int ram=16;
+		double screenSize=15.6;
+		
+		System.out.println("color="+color);
+		System.out.println("price"+price);
+		System.out.println("brand="+brand);
+		System.out.println("model="+model);
+		System.out.println("ram="+ram);
+		System.out.println("screenSize="+screenSize);
+	}
+}
