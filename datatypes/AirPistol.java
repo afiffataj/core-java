638838133b12efbb6ef7b3ba10fafa33b869dd03
afiffataj <afiffataj@gmail.com>
2026-09-14 03:18:26 +0530

    variables

---
+++ datatypes/AirPistol.java
@@ -0,0 +1,26 @@
+class AirPistol{
+	public static void main(String[] args){
+		System.out.println("AirPistol is a toy");
+		
+	String color="Black";
+    String brand="Generic";
+    byte unitCount=1;
+    int capacity=12;
+    short weight=100;
+    long contactNo=	9953900020l;
+	float components=15.45f;
+	String style="Game";
+	boolean areBatteriesRequired=false;
+	
+	System.out.println("color="+color);
+	System.out.println("brand="+brand);
+	System.out.println("unitCount="+unitCount);
+	System.out.println("capacity="+capacity);
+	System.out.println("weight="+weight);
+	System.out.println("contactNo="+contactNo);
+	System.out.println("components="+components);
+	System.out.println("style="+style);
+	System.out.println("areBatteriesRequired="+areBatteriesRequired);
+	}
+}
+	
