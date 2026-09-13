85126b03e25a524133edc63c2f1d111e935fa72c
afiffataj <afiffataj@gmail.com>
2026-09-13 15:58:33 +0530

    variables

---
+++ Clothes.java
@@ -0,0 +1,21 @@
+class Clothes{
+	public static void main(String[] args){
+		System.out.println("Clothes are of designed");
+		
+		String brand="Allen Solly";
+		String type="t-shirt";
+		String color="Blue";
+		String size="Medium";
+		String material="cotton";
+		int price=677;
+		
+		System.out.println("brand="+brand);
+		System.out.println("type="+type);
+		System.out.println("color="+color);
+		System.out.println("size="+size);
+		System.out.println("material="+material);
+		System.out.println("price="+price);
+		
+   }
+}
+		
