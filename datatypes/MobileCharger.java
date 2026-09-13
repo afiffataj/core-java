1fa1a7ef793e143fde0c543be203f6a9abe5375a
afiffataj <afiffataj@gmail.com>
2026-09-14 02:12:06 +0530

    variables

---
+++ datatypes/MobileCharger.java
@@ -0,0 +1,27 @@
+class MobileCharger{
+	public static void main(String[] args){
+		System.out.println("MobileCharger is used for charging");
+		
+       String brand="UN1QUE";
+	   int inputVoltage=222;
+	   byte noOfItems=1;
+	   float itemWeight=0.07f;
+	   double length=30.8d;
+	   short outputCurrent=7;
+	   long pincode=560064l;
+	   boolean isSafe=true;
+	   char size='L';
+	   
+	   System.out.println("brand="+brand);
+	   System.out.println("inputVoltage="+inputVoltage);
+	   System.out.println("noOfItems="+noOfItems);
+	   System.out.println("itemWeight="+itemWeight);
+	   System.out.println("length="+length);
+	   System.out.println("outputCurrent="+outputCurrent);
+	   System.out.println("pincode="+pincode);
+	   System.out.println("isSafe="+isSafe);
+	   System.out.println("size="+size);
+	}
+}
+	   
+	   
