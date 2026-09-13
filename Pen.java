70a7d15634f731dd8d79de74ddc8aa337da5bb19
afiffataj <afiffataj@gmail.com>
2026-09-13 15:07:12 +0530

    variables

---
+++ Pen.java
@@ -0,0 +1,23 @@
+class Pen{
+	public static void main(String[] args){
+		System.out.println("pen is used for writing");
+		
+		String brand="Exel";
+		String color="black";
+		String type="ball pen";
+		int price=15;
+		int quantity=3;
+		double length=15;
+		
+		System.out.println("brand="+brand);
+		System.out.println("color="+color);
+		System.out.println("type="+type);
+		System.out.println("price="+price);
+		System.out.println("quantity="+quantity);
+		System.out.println("length="+length);
+	}
+}
+
+		
+		
+		
