bffe394a09b1e2334360ff4653aecdcea7be0947
afiffataj <afiffataj@gmail.com>
2026-09-13 15:54:41 +0530

    variables

---
+++ ElectricChair.java
@@ -0,0 +1,19 @@
+class ElectricChair{
+	public static void main(String[] args){
+		System.out.println("ElectricChair is useful");
+		
+		String brand="Pride Mobility";
+		String model="Jazzy Air";
+		int price=40000;
+		String batteryType="Lithum-ion";
+		double weight=120;
+		String color="White";
+		
+		System.out.println("brand="+brand);
+		System.out.println("model="+model);
+		System.out.println("price="+price);
+		System.out.println("batteryType="+batteryType);
+		System.out.println("weight="+weight);
+		System.out.println("color="+color);
+	}
+}
