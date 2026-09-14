e7f728e2ec151bcc9fc6c2c311b014a34d969f3b
afiffataj <afiffataj@gmail.com>
2026-09-15 00:04:07 +0530

    variables

---
+++ datatypes/VivoPhone.java
@@ -0,0 +1,27 @@
+class VivoPhone {
+    public static void main(String[] args) {
+        System.out.println("Vivo is a smartphone");
+
+        String color = "Titanium";
+        String brand = "Vivo";
+        byte cameras = 3;
+        int storage = 256;
+        short weight = 189;
+        long productNo = 9876543214L;
+        float price = 42999.50f;
+        String model = "Vivo V50";
+        char grade = 'A';
+        boolean is5G = true;
+
+        System.out.println("color=" + color);
+        System.out.println("brand=" + brand);
+        System.out.println("cameras=" + cameras);
+        System.out.println("storage=" + storage);
+        System.out.println("weight=" + weight);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("model=" + model);
+        System.out.println("grade=" + grade);
+        System.out.println("is5G=" + is5G);
+    }
+}
