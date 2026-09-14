9e600284cd23e01359082cd93189ad17c1b36fad
afiffataj <afiffataj@gmail.com>
2026-09-15 00:04:53 +0530

    variables

---
+++ datatypes/TitanWatch.java
@@ -0,0 +1,27 @@
+class TitanWatch {
+    public static void main(String[] args) {
+        System.out.println("Titan Watch shows time");
+
+        String color = "Brown";
+        String brand = "Titan";
+        byte buttons = 2;
+        int warranty = 24;
+        short weight = 85;
+        long productNo = 32345678902L;
+        float price = 5999.75f;
+        String model = "Titan Edge";
+        char grade = 'A';
+        boolean waterproof = true;
+
+        System.out.println("color=" + color);
+        System.out.println("brand=" + brand);
+        System.out.println("buttons=" + buttons);
+        System.out.println("warranty=" + warranty);
+        System.out.println("weight=" + weight);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("model=" + model);
+        System.out.println("grade=" + grade);
+        System.out.println("waterproof=" + waterproof);
+    }
+}
