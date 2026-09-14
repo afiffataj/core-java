2fe6effbd1ab2c704530297ad05b658f52f8e793
afiffataj <afiffataj@gmail.com>
2026-09-15 00:08:16 +0530

    variables

---
+++ datatypes/SugarEyeliner.java
@@ -0,0 +1,27 @@
+class SugarEyeliner {
+    public static void main(String[] args) {
+        System.out.println("Sugar Eyeliner is a makeup product");
+
+        String color = "Black";
+        String brand = "Sugar";
+        byte quantity = 1;
+        int stock = 60;
+        short volume = 5;
+        long productNo = 12345678904L;
+        float price = 499.25f;
+        String type = "Liquid";
+        char grade = 'B';
+        boolean smudgeProof = true;
+
+        System.out.println("color=" + color);
+        System.out.println("brand=" + brand);
+        System.out.println("quantity=" + quantity);
+        System.out.println("stock=" + stock);
+        System.out.println("volume=" + volume);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("type=" + type);
+        System.out.println("grade=" + grade);
+        System.out.println("smudgeProof=" + smudgeProof);
+    }
+}
