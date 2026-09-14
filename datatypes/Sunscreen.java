a4d1cbd8960c847169f2bf5a9352b8db1a413628
afiffataj <afiffataj@gmail.com>
2026-09-15 00:06:51 +0530

    variables

---
+++ datatypes/Sunscreen.java
@@ -0,0 +1,27 @@
+class Sunscreen {
+    public static void main(String[] args) {
+        System.out.println("Sunscreen protects the skin");
+
+        String color = "White";
+        String brand = "Minimalist";
+        byte quantity = 1;
+        int stock = 100;
+        short volume = 50;
+        long productNo = 12345678908L;
+        float price = 499.50f;
+        String protection = "SPF 50";
+        char grade = 'A';
+        boolean waterResistant = true;
+
+        System.out.println("color=" + color);
+        System.out.println("brand=" + brand);
+        System.out.println("quantity=" + quantity);
+        System.out.println("stock=" + stock);
+        System.out.println("volume=" + volume);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("protection=" + protection);
+        System.out.println("grade=" + grade);
+        System.out.println("waterResistant=" + waterResistant);
+    }
+}
