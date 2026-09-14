9e59d04049de3a5c2ec07f7d1822710e0b6bd7cc
afiffataj <afiffataj@gmail.com>
2026-09-15 00:02:18 +0530

    variables

---
+++ datatypes/ZaraHandbag.java
@@ -0,0 +1,27 @@
+class ZaraHandbag {
+    public static void main(String[] args) {
+        System.out.println("Zara Handbag is a fashion accessory");
+
+        String color = "Brown";
+        String brand = "Zara";
+        byte pockets = 4;
+        int stock = 40;
+        short weight = 700;
+        long productNo = 33333333302L;
+        float price = 2999.50f;
+        String material = "Leather";
+        char grade = 'A';
+        boolean shoulderStrap = true;
+
+        System.out.println("color=" + color);
+        System.out.println("brand=" + brand);
+        System.out.println("pockets=" + pockets);
+        System.out.println("stock=" + stock);
+        System.out.println("weight=" + weight);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("material=" + material);
+        System.out.println("grade=" + grade);
+        System.out.println("shoulderStrap=" + shoulderStrap);
+    }
+}
