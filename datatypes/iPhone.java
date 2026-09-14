5df9eb23401b18cf9762fcfbf19fcd9dc7509742
afiffataj <afiffataj@gmail.com>
2026-09-15 00:00:37 +0530

    variables

---
+++ datatypes/iPhone.java
@@ -0,0 +1,27 @@
+class iPhone {
+    public static void main(String[] args) {
+        System.out.println("iPhone is a smartphone");
+
+        String color = "White";
+        String brand = "Apple";
+        byte cameras = 2;
+        int storage = 128;
+        short weight = 171;
+        long productNo = 9876543211L;
+        float price = 69999.50f;
+        String model = "iPhone 16";
+        char grade = 'A';
+        boolean is5G = true;
+
+        System.out.println("color=" + color);
+        System.out.println("brand=" + brand);
+        System.out.println("cameras=" + cameras);
+        System.out.println("storage=" + storage);
+        System.out.println("weight=" + weight);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("model=" + model);
+        System.out.println("grade=" + grade);
+        System.out.println("is5G=" + is5G);
+    }
+}
