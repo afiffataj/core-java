2a2c0eb44df6e3b5366fffe267283406cb57156c
afiffataj <afiffataj@gmail.com>
2026-09-15 00:05:43 +0530

    variables

---
+++ datatypes/TeddyBear.java
@@ -0,0 +1,27 @@
+class TeddyBear {
+    public static void main(String[] args) {
+        System.out.println("Teddy Bear is a soft toy");
+
+        String color = "Brown";
+        String brand = "Hamleys";
+        byte age = 3;
+        int stock = 55;
+        short height = 40;
+        long productNo = 55555555517L;
+        float price = 899.50f;
+        String type = "Soft Toy";
+        char grade = 'A';
+        boolean washable = true;
+
+        System.out.println("color=" + color);
+        System.out.println("brand=" + brand);
+        System.out.println("age=" + age);
+        System.out.println("stock=" + stock);
+        System.out.println("height=" + height);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("type=" + type);
+        System.out.println("grade=" + grade);
+        System.out.println("washable=" + washable);
+    }
+}
