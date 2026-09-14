41487cc61c3faa402ea44fc7ab8150e4b6fd23c4
afiffataj <afiffataj@gmail.com>
2026-09-15 00:03:19 +0530

    variables

---
+++ datatypes/WildcraftBag.java
@@ -0,0 +1,27 @@
+class WildcraftBag {
+    public static void main(String[] args) {
+        System.out.println("Wildcraft Bag is a travel product");
+
+        String color = "Black";
+        String brand = "Wildcraft";
+        byte pockets = 6;
+        int capacity = 30;
+        short weight = 900;
+        long productNo = 62345678902L;
+        float price = 1999.75f;
+        String model = "Backpack";
+        char grade = 'A';
+        boolean waterproof = true;
+
+        System.out.println("color=" + color);
+        System.out.println("brand=" + brand);
+        System.out.println("pockets=" + pockets);
+        System.out.println("capacity=" + capacity);
+        System.out.println("weight=" + weight);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("model=" + model);
+        System.out.println("grade=" + grade);
+        System.out.println("waterproof=" + waterproof);
+    }
+}
