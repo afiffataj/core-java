b452c0ea3a8bbb87df63356cc283eed78136b763
afiffataj <afiffataj@gmail.com>
2026-09-14 14:04:59 +0530

    variables

---
+++ datatypes/Kite.java
@@ -0,0 +1,26 @@
+class Kite{
+	public static void main(String[] args){
+      System.out.println("Kite is made up of paper");
+
+      String brand="Ruhani";
+      String color="Red";
+      int price=224;
+      byte size=42;
+      short discount=68;
+      boolean isAvailable=true;
+      float stars=2.8f;
+      long pincode=560065l;
+      double length=45.9d;
+
+      System.out.println("brand="+brand);
+	  System.out.println("color="+color);
+	  System.out.println("price="+price);
+	  System.out.println("size="+size);
+	  System.out.println("discount="+discount);
+	  System.out.println("isAvailable="+isAvailable);
+	  System.out.println("stars="+stars);
+	  System.out.println("pincode="+pincode);
+	  System.out.println("length="+length);
+	}
+}
+ 	  
