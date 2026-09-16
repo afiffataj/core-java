f5f7d878ccdaef40b9c8a4e0ba79c102b8917496
afiffataj <afiffataj@gmail.com>
2026-09-16 17:26:30 +0530

    variables

---
+++ datatypes/SonyHeadphones.java
@@ -0,0 +1,27 @@
+class SonyHeadphones {
+    public static void main(String[] args) {
+        System.out.println("Sony Headphones produce sound");
+
+        String color = "Black";
+        String brand = "Sony";
+        byte speakers = 2;
+        int battery = 30;
+        short weight = 250;
+        long productNo = 22345678902L;
+        float price = 24999.50f;
+        String model = "WH Series";
+        char grade = 'A';
+        boolean noiseCancellation = true;
+
+        System.out.println("color=" + color);
+        System.out.println("brand=" + brand);
+        System.out.println("speakers=" + speakers);
+        System.out.println("battery=" + battery);
+        System.out.println("weight=" + weight);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("model=" + model);
+        System.out.println("grade=" + grade);
+        System.out.println("noiseCancellation=" + noiseCancellation);
+    }
+}
