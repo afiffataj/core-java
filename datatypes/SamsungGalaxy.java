e9e1f39d8f69cd1dcc387cf06eac2e476269e741
afiffataj <afiffataj@gmail.com>
2026-09-16 17:29:01 +0530

    variables

---
+++ datatypes/SamsungGalaxy.java
@@ -0,0 +1,27 @@
+class SamsungGalaxy{
+    public static void main(String[] args){
+        System.out.println("Samsung Galaxy is a smartphone");
+
+        String color="Black";
+        String brand="Samsung";
+        byte unitCount=1;
+        int capacity=256;
+        short weight=168;
+        long contactNo=9876543210L;
+        float price=74999.50f;
+        String style="Premium";
+        char grade='A';
+        boolean is5G=true;
+
+        System.out.println("color="+color);
+        System.out.println("brand="+brand);
+        System.out.println("unitCount="+unitCount);
+        System.out.println("capacity="+capacity);
+        System.out.println("weight="+weight);
+        System.out.println("contactNo="+contactNo);
+        System.out.println("price="+price);
+        System.out.println("style="+style);
+        System.out.println("grade="+grade);
+        System.out.println("is5G="+is5G);
+    }
+}
