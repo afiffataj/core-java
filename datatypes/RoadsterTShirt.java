c3891d071a7110c5d8382666eadd93172ae5e138
afiffataj <afiffataj@gmail.com>
2026-09-16 17:32:01 +0530

    variables

---
+++ datatypes/RoadsterTShirt.java
@@ -0,0 +1,27 @@
+class RoadsterTShirt {
+    public static void main(String[] args) {
+        System.out.println("Roadster T-Shirt is casual clothing");
+
+        String color = "Black";
+        String brand = "Roadster";
+        byte size = 40;
+        int stock = 120;
+        short length = 28;
+        long productNo = 52345678902L;
+        float price = 799.50f;
+        String model = "Printed T-Shirt";
+        char grade = 'B';
+        boolean cotton = true;
+
+        System.out.println("color=" + color);
+        System.out.println("brand=" + brand);
+        System.out.println("size=" + size);
+        System.out.println("stock=" + stock);
+        System.out.println("length=" + length);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("model=" + model);
+        System.out.println("grade=" + grade);
+        System.out.println("cotton=" + cotton);
+    }
+}
