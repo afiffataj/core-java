caae47a240968b10075fad7cf4fa7c812143305d
afiffataj <afiffataj@gmail.com>
2026-09-16 17:34:13 +0530

    variables

---
+++ datatypes/RemoteControlCar.java
@@ -0,0 +1,27 @@
+class RemoteControlCar {
+    public static void main(String[] args) {
+        System.out.println("Remote Control Car is an electronic toy");
+
+        String color = "Red";
+        String brand = "Funskool";
+        byte wheels = 4;
+        int stock = 40;
+        short speed = 15;
+        long productNo = 55555555512L;
+        float price = 1299.50f;
+        String type = "Remote Car";
+        char grade = 'A';
+        boolean rechargeable = true;
+
+        System.out.println("color=" + color);
+        System.out.println("brand=" + brand);
+        System.out.println("wheels=" + wheels);
+        System.out.println("stock=" + stock);
+        System.out.println("speed=" + speed);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("type=" + type);
+        System.out.println("grade=" + grade);
+        System.out.println("rechargeable=" + rechargeable);
+    }
+}
