5748009542e6a531309f8e041d5975a4b3fb8dbf
afiffataj <afiffataj@gmail.com>
2026-09-16 17:33:01 +0530

    variables

---
+++ datatypes/RedmiPhone.java
@@ -0,0 +1,27 @@
+class RedmiPhone {
+    public static void main(String[] args) {
+        System.out.println("Redmi is a smartphone");
+
+        String color = "Green";
+        String brand = "Redmi";
+        byte cameras = 3;
+        int storage = 128;
+        short weight = 190;
+        long productNo = 9876543213L;
+        float price = 22999.25f;
+        String model = "Redmi Note";
+        char grade = 'B';
+        boolean is5G = true;
+
+        System.out.println("color=" + color);
+        System.out.println("brand=" + brand);
+        System.out.println("cameras=" + cameras);
+        System.out.println("storage=" + storage);
+        System.out.println("weight=" + weight);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("model=" + model);
+        System.out.println("grade=" + grade);
+        System.out.println("is5G=" + is5G);
+    }
+}
