a59d0909b9beca1bf9c40beb45a57e1b78c29b72
afiffataj <afiffataj@gmail.com>
2026-09-16 17:27:20 +0530

    variables

---
+++ datatypes/ScrabbleGame.java
@@ -0,0 +1,27 @@
+class ScrabbleGame {
+    public static void main(String[] args) {
+        System.out.println("Scrabble is a word board game");
+
+        String color = "Blue";
+        String brand = "Mattel";
+        byte players = 4;
+        int stock = 45;
+        short tiles = 100;
+        long productNo = 55555555511L;
+        float price = 799.50f;
+        String type = "Word Game";
+        char grade = 'A';
+        boolean educational = true;
+
+        System.out.println("color=" + color);
+        System.out.println("brand=" + brand);
+        System.out.println("players=" + players);
+        System.out.println("stock=" + stock);
+        System.out.println("tiles=" + tiles);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("type=" + type);
+        System.out.println("grade=" + grade);
+        System.out.println("educational=" + educational);
+    }
+}
