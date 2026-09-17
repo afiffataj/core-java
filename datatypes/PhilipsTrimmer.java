12cbd79b6b4502dbfdecf2a1511c0041e71fac0b
afiffataj <afiffataj@gmail.com>
2026-09-17 21:09:05 +0530

    variables

---
+++ datatypes/PhilipsTrimmer.java
@@ -0,0 +1,27 @@
+class PhilipsTrimmer {
+    public static void main(String[] args) {
+        System.out.println("Philips Trimmer is a grooming product");
+
+        String color = "Black";
+        String brand = "Philips";
+        byte settings = 10;
+        int battery = 60;
+        short weight = 250;
+        long productNo = 82345678901L;
+        float price = 2499.50f;
+        String model = "Beard Trimmer";
+        char grade = 'A';
+        boolean rechargeable = true;
+
+        System.out.println("color=" + color);
+        System.out.println("brand=" + brand);
+        System.out.println("settings=" + settings);
+        System.out.println("battery=" + battery);
+        System.out.println("weight=" + weight);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("model=" + model);
+        System.out.println("grade=" + grade);
+        System.out.println("rechargeable=" + rechargeable);
+    }
+}
