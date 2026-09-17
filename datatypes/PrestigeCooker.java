40667ddf4e6c0d24da5956cf9f9428ca9291a0d4
afiffataj <afiffataj@gmail.com>
2026-09-17 21:08:16 +0530

    variables

---
+++ datatypes/PrestigeCooker.java
@@ -0,0 +1,27 @@
+class PrestigeCooker {
+    public static void main(String[] args) {
+        System.out.println("Prestige Cooker is a kitchen product");
+
+        String color = "Silver";
+        String brand = "Prestige";
+        byte whistles = 5;
+        int capacity = 5;
+        short weight = 2500;
+        long productNo = 92345678901L;
+        float price = 2999.50f;
+        String model = "Pressure Cooker";
+        char grade = 'A';
+        boolean induction = true;
+
+        System.out.println("color=" + color);
+        System.out.println("brand=" + brand);
+        System.out.println("whistles=" + whistles);
+        System.out.println("capacity=" + capacity);
+        System.out.println("weight=" + weight);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("model=" + model);
+        System.out.println("grade=" + grade);
+        System.out.println("induction=" + induction);
+    }
+}
