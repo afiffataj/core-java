dc6486bf946b80cacd02084e64fe307161c98f85
afiffataj <afiffataj@gmail.com>
2026-09-17 21:14:01 +0530

    variables

---
+++ datatypes/MinimalistSerum.java
@@ -0,0 +1,27 @@
+class MinimalistSerum {
+    public static void main(String[] args) {
+        System.out.println("Minimalist Serum is a skincare product");
+
+        String color = "Transparent";
+        String brand = "Minimalist";
+        byte quantity = 1;
+        int stock = 70;
+        short volume = 30;
+        long productNo = 12345678907L;
+        float price = 599.50f;
+        String ingredient = "Niacinamide";
+        char grade = 'A';
+        boolean fragranceFree = true;
+
+        System.out.println("color=" + color);
+        System.out.println("brand=" + brand);
+        System.out.println("quantity=" + quantity);
+        System.out.println("stock=" + stock);
+        System.out.println("volume=" + volume);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("ingredient=" + ingredient);
+        System.out.println("grade=" + grade);
+        System.out.println("fragranceFree=" + fragranceFree);
+    }
+}
