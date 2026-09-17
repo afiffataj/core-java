699f230ddc51fa78e6d60ddbfe3a97fd513ec67c
afiffataj <afiffataj@gmail.com>
2026-09-17 21:11:33 +0530

    variables

---
+++ datatypes/NikeShoes.java
@@ -0,0 +1,28 @@
+class NikeShoes{
+    public static void main(String[] args){
+
+        System.out.println("Nike Shoes are sports footwear");
+
+        String color="White";
+        String brand="Nike";
+        byte unitCount=1;
+        int size=9;
+        short weight=650;
+        long productNo=9876543210L;
+        float price=5999.99f;
+        String style="Running";
+        char grade='A';
+        boolean isWaterproof=false;
+
+        System.out.println("color="+color);
+        System.out.println("brand="+brand);
+        System.out.println("unitCount="+unitCount);
+        System.out.println("size="+size);
+        System.out.println("weight="+weight);
+        System.out.println("productNo="+productNo);
+        System.out.println("price="+price);
+        System.out.println("style="+style);
+        System.out.println("grade="+grade);
+        System.out.println("isWaterproof="+isWaterproof);
+    }
+}
