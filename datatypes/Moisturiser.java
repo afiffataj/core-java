b7396338b4fd961bfed27ea5e37ed0eec968d76f
afiffataj <afiffataj@gmail.com>
2026-09-17 21:12:24 +0530

    variables

---
+++ datatypes/Moisturiser.java
@@ -0,0 +1,67 @@
+class Moisturiser{
+	public static void main(String[] args){
+		System.out.println("Moisturiser makes the face softer");
+		
+		String color="Green";
+		String brand="Cetaphil";
+		float itemVolume=8.8f;
+		double dimensions=9.8d;
+		byte noOfItem=1;
+		int price=2457;
+		char size='L';
+		boolean isAvailable=true;
+		long pincode=560056l;
+		
+		System.out.println("color before re-initalization="+color);
+		System.out.println("brand before re-initalization="+brand);
+		System.out.println("itemVolume before re-initalization="+itemVolume);
+		System.out.println("dimensions before re-initalization="+dimensions);
+		System.out.println("noOfItem before re-initalization="+noOfItem);
+		System.out.println("price before re-initalization="+price);
+		System.out.println("size before re-initalization="+size);
+		System.out.println("isAvailable before re-initalization="+isAvailable);
+		System.out.println("pincode before re-initalization="+pincode);
+		System.out.println();
+		
+		color="Pink";
+		brand="Ponds";
+		itemVolume=7.5f;
+		dimensions=6.7d;
+		noOfItem=6;
+		price=1500;
+		size='L';
+		isAvailable=false;
+		pincode=560086l;
+		
+		System.out.println("color after re-initalization="+color);
+		System.out.println("brand after re-initalization="+brand);
+		System.out.println("itemVolume after re-initalization="+itemVolume);
+		System.out.println("dimensions after re-initalization="+dimensions);
+		System.out.println("noOfItem after re-initalization="+noOfItem);
+		System.out.println("price after re-initalization="+price);
+		System.out.println("size after re-initalization="+size);
+		System.out.println("isAvailable after re-initalization="+isAvailable);
+		System.out.println("pincode after re-initalization="+pincode);
+		System.out.println();
+		
+		color="Blue";
+		brand="Nivea";
+		itemVolume=9.5f;
+		dimensions=8.7d;
+		noOfItem=5;
+		price=1670;
+		size='L';
+		isAvailable=false;
+	    pincode=560064l;
+		
+		System.out.println("color after re-initalization="+color);
+		System.out.println("brand after re-initalization="+brand);
+		System.out.println("itemVolume after re-initalization="+itemVolume);
+		System.out.println("dimensions after re-initalization="+dimensions);
+		System.out.println("noOfItem after re-initalization="+noOfItem);
+		System.out.println("price after re-initalization="+price);
+		System.out.println("size after re-initalization="+size);
+		System.out.println("isAvailable after re-initalization="+isAvailable);
+		System.out.println("pincode after re-initalization="+pincode);
+	}
+}
