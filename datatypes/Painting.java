80a60fea65f20524791ea8fb12e5cec4451a479f
afiffataj <afiffataj@gmail.com>
2026-09-17 21:09:47 +0530

    variables

---
+++ datatypes/Painting.java
@@ -0,0 +1,23 @@
+class Painting{
+	public static void main(String[] args){
+		System.out.println("Painting is need for wall");
+		
+		int price=2099;
+		String orientation="Portrait";
+		String theme="Nature";
+		byte noOfItems=1;
+		short size=30;
+		boolean isFramed=true;
+		float discount=58f;
+		long areaNo=123769l;
+		
+		System.out.println("price="+price);
+		System.out.println("orientation="+orientation);
+		System.out.println("theme="+theme);
+		System.out.println("noOfItems="+noOfItems);
+		System.out.println("size="+size);
+		System.out.println("isFramed="+isFramed);
+		System.out.println("discount="+discount);
+		System.out.println("areaNo="+areaNo);
+	}
+}
