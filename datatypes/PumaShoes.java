5664875741141c0710c81ed1f1d4e309c5812c51
afiffataj <afiffataj@gmail.com>
2026-09-17 21:07:18 +0530

    variables

---
+++ datatypes/PumaShoes.java
@@ -0,0 +1,27 @@
+class PumaShoes {
+    public static void main(String[] args) {
+        System.out.println("Puma Shoes are footwear");
+
+        String color = "Red";
+        String brand = "Puma";
+        byte size = 9;
+        int stock = 60;
+        short weight = 600;
+        long productNo = 42345678903L;
+        float price = 3999.75f;
+        String model = "Running Shoes";
+        char grade = 'B';
+        boolean sports = true;
+
+        System.out.println("color=" + color);
+        System.out.println("brand=" + brand);
+        System.out.println("size=" + size);
+        System.out.println("stock=" + stock);
+        System.out.println("weight=" + weight);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("model=" + model);
+        System.out.println("grade=" + grade);
+        System.out.println("sports=" + sports);
+    }
+}
