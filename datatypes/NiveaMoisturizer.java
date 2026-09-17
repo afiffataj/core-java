e2355c01cfaf98f40712e62417b69551e09d9f4b
afiffataj <afiffataj@gmail.com>
2026-09-17 21:10:42 +0530

    variables

---
+++ datatypes/NiveaMoisturizer.java
@@ -0,0 +1,27 @@
+class NiveaMoisturizer {
+    public static void main(String[] args) {
+        System.out.println("Nivea Moisturizer is a skincare product");
+
+        String color = "White";
+        String brand = "Nivea";
+        byte quantity = 1;
+        int stock = 90;
+        short volume = 200;
+        long productNo = 12345678906L;
+        float price = 349.75f;
+        String type = "Hydrating";
+        char grade = 'A';
+        boolean moisturizing = true;
+
+        System.out.println("color=" + color);
+        System.out.println("brand=" + brand);
+        System.out.println("quantity=" + quantity);
+        System.out.println("stock=" + stock);
+        System.out.println("volume=" + volume);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("type=" + type);
+        System.out.println("grade=" + grade);
+        System.out.println("moisturizing=" + moisturizing);
+    }
+}
