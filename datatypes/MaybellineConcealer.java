cea91b9ed9aacff2d01440a97c644550b1c8e842
afiffataj <afiffataj@gmail.com>
2026-09-18 09:12:55 +0530

    variables

---
+++ datatypes/MaybellineConcealer.java
@@ -0,0 +1,27 @@
+class MaybellineConcealer {
+    public static void main(String[] args) {
+        System.out.println("Maybelline Concealer is a makeup product");
+
+        String shade = "Medium";
+        String brand = "Maybelline";
+        byte quantity = 1;
+        int stock = 90;
+        short volume = 10;
+        long productNo = 11111111103L;
+        float price = 649.50f;
+        String coverage = "Full";
+        char grade = 'A';
+        boolean waterproof = true;
+
+        System.out.println("shade=" + shade);
+        System.out.println("brand=" + brand);
+        System.out.println("quantity=" + quantity);
+        System.out.println("stock=" + stock);
+        System.out.println("volume=" + volume);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("coverage=" + coverage);
+        System.out.println("grade=" + grade);
+        System.out.println("waterproof=" + waterproof);
+    }
+}
