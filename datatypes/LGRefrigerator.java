846e93e2831343a37b81b22a356b1c57ac62ee58
afiffataj <afiffataj@gmail.com>
2026-09-18 09:17:38 +0530

    variables

---
+++ datatypes/LGRefrigerator.java
@@ -0,0 +1,27 @@
+class LGRefrigerator {
+    public static void main(String[] args) {
+        System.out.println("LG Refrigerator stores food");
+
+        String color = "Silver";
+        String brand = "LG";
+        byte doors = 2;
+        int capacity = 350;
+        short weight = 6500;
+        long productNo = 72345678902L;
+        float price = 44999.75f;
+        String model = "Double Door";
+        char grade = 'A';
+        boolean inverter = true;
+
+        System.out.println("color=" + color);
+        System.out.println("brand=" + brand);
+        System.out.println("doors=" + doors);
+        System.out.println("capacity=" + capacity);
+        System.out.println("weight=" + weight);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("model=" + model);
+        System.out.println("grade=" + grade);
+        System.out.println("inverter=" + inverter);
+    }
+}
