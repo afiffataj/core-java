9871c6ab29c965cf67c5faf9980d42abb46e1879
afiffataj <afiffataj@gmail.com>
2026-09-18 09:16:35 +0530

    variables

---
+++ datatypes/LakmeFoundation.java
@@ -0,0 +1,27 @@
+class LakmeFoundation {
+    public static void main(String[] args) {
+        System.out.println("Lakme Foundation is a makeup product");
+
+        String shade = "Natural";
+        String brand = "Lakme";
+        byte quantity = 1;
+        int stock = 50;
+        short volume = 30;
+        long productNo = 12345678901L;
+        float price = 599.50f;
+        String finish = "Matte";
+        char grade = 'A';
+        boolean waterproof = true;
+
+        System.out.println("shade=" + shade);
+        System.out.println("brand=" + brand);
+        System.out.println("quantity=" + quantity);
+        System.out.println("stock=" + stock);
+        System.out.println("volume=" + volume);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("finish=" + finish);
+        System.out.println("grade=" + grade);
+        System.out.println("waterproof=" + waterproof);
+    }
+}
