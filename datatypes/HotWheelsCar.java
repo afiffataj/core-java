d0a7015e56c67f052fe642af9322389e2e4ee514
afiffataj <afiffataj@gmail.com>
2026-09-18 09:20:32 +0530

    variables

---
+++ datatypes/HotWheelsCar.java
@@ -0,0 +1,27 @@
+class HotWheelsCar {
+    public static void main(String[] args) {
+        System.out.println("Hot Wheels Car is a toy vehicle");
+
+        String color = "Red";
+        String brand = "Hot Wheels";
+        byte wheels = 4;
+        int stock = 100;
+        short weight = 50;
+        long productNo = 55555555502L;
+        float price = 199.50f;
+        String type = "Toy Car";
+        char grade = 'A';
+        boolean metalBody = true;
+
+        System.out.println("color=" + color);
+        System.out.println("brand=" + brand);
+        System.out.println("wheels=" + wheels);
+        System.out.println("stock=" + stock);
+        System.out.println("weight=" + weight);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("type=" + type);
+        System.out.println("grade=" + grade);
+        System.out.println("metalBody=" + metalBody);
+    }
+}
