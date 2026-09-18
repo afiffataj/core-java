fbe379a35bb48e9951aa8719ff10107c48a18cf0
afiffataj <afiffataj@gmail.com>
2026-09-18 09:11:59 +0530

    variables

---
+++ datatypes/MaybellineLipstick.java
@@ -0,0 +1,27 @@
+class MaybellineLipstick {
+    public static void main(String[] args) {
+        System.out.println("Maybelline Lipstick is a makeup product");
+
+        String color = "Red";
+        String brand = "Maybelline";
+        byte quantity = 1;
+        int stock = 100;
+        short weight = 25;
+        long productNo = 12345678902L;
+        float price = 499.75f;
+        String finish = "Matte";
+        char grade = 'A';
+        boolean longLasting = true;
+
+        System.out.println("color=" + color);
+        System.out.println("brand=" + brand);
+        System.out.println("quantity=" + quantity);
+        System.out.println("stock=" + stock);
+        System.out.println("weight=" + weight);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("finish=" + finish);
+        System.out.println("grade=" + grade);
+        System.out.println("longLasting=" + longLasting);
+    }
+}
