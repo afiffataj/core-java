8cfe1df179165543e10d76bd0b1a2ac64d3668e7
afiffataj <afiffataj@gmail.com>
2026-09-18 09:14:28 +0530

    variables

---
+++ datatypes/LevisJeans.java
@@ -0,0 +1,27 @@
+class LevisJeans {
+    public static void main(String[] args) {
+        System.out.println("Levis Jeans are clothing");
+
+        String color = "Blue";
+        String brand = "Levis";
+        byte size = 32;
+        int stock = 100;
+        short length = 32;
+        long productNo = 52345678901L;
+        float price = 2499.50f;
+        String model = "501 Original";
+        char grade = 'A';
+        boolean stretchable = true;
+
+        System.out.println("color=" + color);
+        System.out.println("brand=" + brand);
+        System.out.println("size=" + size);
+        System.out.println("stock=" + stock);
+        System.out.println("length=" + length);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("model=" + model);
+        System.out.println("grade=" + grade);
+        System.out.println("stretchable=" + stretchable);
+    }
+}
