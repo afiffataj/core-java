7a3d6fbd2774a04cb85450ebd9c019c6d76508dd
afiffataj <afiffataj@gmail.com>
2026-09-18 09:13:41 +0530

    variables

---
+++ datatypes/LorealMascara.java
@@ -0,0 +1,27 @@
+class LorealMascara {
+    public static void main(String[] args) {
+        System.out.println("Loreal Mascara is a makeup product");
+
+        String color = "Black";
+        String brand = "Loreal";
+        byte quantity = 1;
+        int stock = 80;
+        short volume = 10;
+        long productNo = 12345678903L;
+        float price = 699.50f;
+        String type = "Volume";
+        char grade = 'A';
+        boolean waterproof = true;
+
+        System.out.println("color=" + color);
+        System.out.println("brand=" + brand);
+        System.out.println("quantity=" + quantity);
+        System.out.println("stock=" + stock);
+        System.out.println("volume=" + volume);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("type=" + type);
+        System.out.println("grade=" + grade);
+        System.out.println("waterproof=" + waterproof);
+    }
+}
