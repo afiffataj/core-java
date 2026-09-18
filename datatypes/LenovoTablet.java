51ece3cd1efb1d158a12350b4131bbe9d3660d39
afiffataj <afiffataj@gmail.com>
2026-09-18 09:15:40 +0530

    variables

---
+++ datatypes/LenovoTablet.java
@@ -0,0 +1,27 @@
+class LenovoTablet {
+    public static void main(String[] args) {
+        System.out.println("Lenovo Tablet is a portable computer");
+
+        String color = "Grey";
+        String brand = "Lenovo";
+        byte cameras = 2;
+        int storage = 128;
+        short weight = 465;
+        long productNo = 92345678908L;
+        float price = 22999.50f;
+        String model = "Lenovo Tab";
+        char grade = 'A';
+        boolean wifi = true;
+
+        System.out.println("color=" + color);
+        System.out.println("brand=" + brand);
+        System.out.println("cameras=" + cameras);
+        System.out.println("storage=" + storage);
+        System.out.println("weight=" + weight);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("model=" + model);
+        System.out.println("grade=" + grade);
+        System.out.println("wifi=" + wifi);
+    }
+}
