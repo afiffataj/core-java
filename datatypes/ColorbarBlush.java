02aa4580bed67f6a9c4c711ab297d3a91f43b90c
afiffataj <afiffataj@gmail.com>
2026-09-19 13:53:14 +0530

    variables

---
+++ datatypes/ColorbarBlush.java
@@ -0,0 +1,27 @@
+class ColorbarBlush {
+    public static void main(String[] args) {
+        System.out.println("Colorbar Blush is a makeup product");
+
+        String color = "Peach";
+        String brand = "Colorbar";
+        byte quantity = 1;
+        int stock = 65;
+        short weight = 8;
+        long productNo = 11111111104L;
+        float price = 599.25f;
+        String finish = "Shimmer";
+        char grade = 'B';
+        boolean blendable = true;
+
+        System.out.println("color=" + color);
+        System.out.println("brand=" + brand);
+        System.out.println("quantity=" + quantity);
+        System.out.println("stock=" + stock);
+        System.out.println("weight=" + weight);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("finish=" + finish);
+        System.out.println("grade=" + grade);
+        System.out.println("blendable=" + blendable);
+    }
+}
