5e7bb102d88c4aafa17154e7907cece6c3106e9b
afiffataj <afiffataj@gmail.com>
2026-09-19 13:51:36 +0530

    variables

---
+++ datatypes/Disk.java
@@ -0,0 +1,28 @@
+class Disk{
+	public static void main(String[] args){
+		System.out.println("Disk is used to apply on computer");
+		
+		String brand="Kingster";
+		int price=5333;
+		byte discount=10;
+		float hardDisk=3.0f;
+		double digitalStorageCapacity=1000d;
+		short cacheMemory=931;
+		long dimensions=122515l;
+		String color="Black";
+		String installaionType="External Hard Drive";
+		char size='9';
+		
+		System.out.println("brand of disk="+brand);
+		System.out.println("price of disk="+price);
+		System.out.println("discount for disk="+discount);
+		System.out.println("hardDisk of item="+hardDisk);
+		System.out.println("digitalStorageCapacity of disk="+digitalStorageCapacity);
+		System.out.println("cacheMemory of disk="+cacheMemory);
+		System.out.println("dimensions of disk="+dimensions);
+		System.out.println("color of disk="+color);
+		System.out.println("installaionType of disk="+installaionType);
+		System.out.println("size of disk="+size);
+		
+	}
+}
