131a20223f089fed5a4bd53d71996ec84d9dde50
afiffataj <afiffataj@gmail.com>
2026-09-19 13:50:06 +0530

    variables

---
+++ datatypes/DoveConditioner.java
@@ -0,0 +1,27 @@
+class DoveConditioner {
+    public static void main(String[] args) {
+        System.out.println("Dove Conditioner is a haircare product");
+
+        String color = "White";
+        String brand = "Dove";
+        byte quantity = 1;
+        int stock = 75;
+        short volume = 180;
+        long productNo = 22222222202L;
+        float price = 329.75f;
+        String type = "Moisturizing";
+        char grade = 'A';
+        boolean smoothening = true;
+
+        System.out.println("color=" + color);
+        System.out.println("brand=" + brand);
+        System.out.println("quantity=" + quantity);
+        System.out.println("stock=" + stock);
+        System.out.println("volume=" + volume);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("type=" + type);
+        System.out.println("grade=" + grade);
+        System.out.println("smoothening=" + smoothening);
+    }
+}
