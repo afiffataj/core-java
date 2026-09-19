24f2d0c51eb102c2f7be969000f7e514720c0d1b
afiffataj <afiffataj@gmail.com>
2026-09-19 13:54:27 +0530

    variables

---
+++ datatypes/CasioCalculator.java
@@ -0,0 +1,27 @@
+class CasioCalculator {
+    public static void main(String[] args) {
+        System.out.println("Casio Calculator is a stationery product");
+
+        String color = "Black";
+        String brand = "Casio";
+        byte batteries = 1;
+        int functions = 400;
+        short weight = 200;
+        long productNo = 44444444405L;
+        float price = 999.50f;
+        String type = "Scientific";
+        char grade = 'A';
+        boolean solarPowered = true;
+
+        System.out.println("color=" + color);
+        System.out.println("brand=" + brand);
+        System.out.println("batteries=" + batteries);
+        System.out.println("functions=" + functions);
+        System.out.println("weight=" + weight);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("type=" + type);
+        System.out.println("grade=" + grade);
+        System.out.println("solarPowered=" + solarPowered);
+    }
+}
