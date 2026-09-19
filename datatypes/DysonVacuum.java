f082b1bbc459cd7e4f9322c7be2caffbab0f49e7
afiffataj <afiffataj@gmail.com>
2026-09-19 13:47:50 +0530

    variables

---
+++ datatypes/DysonVacuum.java
@@ -0,0 +1,27 @@
+class DysonVacuum {
+    public static void main(String[] args) {
+        System.out.println("Dyson Vacuum Cleaner cleans floors");
+
+        String color = "Purple";
+        String brand = "Dyson";
+        byte modes = 3;
+        int battery = 60;
+        short weight = 2500;
+        long productNo = 82345678902L;
+        float price = 39999.75f;
+        String model = "V Series";
+        char grade = 'A';
+        boolean cordless = true;
+
+        System.out.println("color=" + color);
+        System.out.println("brand=" + brand);
+        System.out.println("modes=" + modes);
+        System.out.println("battery=" + battery);
+        System.out.println("weight=" + weight);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("model=" + model);
+        System.out.println("grade=" + grade);
+        System.out.println("cordless=" + cordless);
+    }
+}
