a43531f6185f5f37942c3cf77bfe4456d5fc08de
afiffataj <afiffataj@gmail.com>
2026-09-19 13:50:52 +0530

    variables

---
+++ datatypes/DisneyPuzzle.java
@@ -0,0 +1,27 @@
+class DisneyPuzzle {
+    public static void main(String[] args) {
+        System.out.println("Disney Puzzle is a puzzle game");
+
+        String color = "Multicolor";
+        String brand = "Disney";
+        byte age = 5;
+        int pieces = 100;
+        short weight = 400;
+        long productNo = 55555555509L;
+        float price = 299.50f;
+        String type = "Jigsaw Puzzle";
+        char grade = 'B';
+        boolean educational = true;
+
+        System.out.println("color=" + color);
+        System.out.println("brand=" + brand);
+        System.out.println("age=" + age);
+        System.out.println("pieces=" + pieces);
+        System.out.println("weight=" + weight);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("type=" + type);
+        System.out.println("grade=" + grade);
+        System.out.println("educational=" + educational);
+    }
+}
