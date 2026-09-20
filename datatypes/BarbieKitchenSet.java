153542261bda9512a9df8b1bc505090f266a3a36
afiffataj <afiffataj@gmail.com>
2026-09-20 23:32:55 +0530

    variables

---
+++ datatypes/BarbieKitchenSet.java
@@ -0,0 +1,27 @@
+class BarbieKitchenSet {
+    public static void main(String[] args) {
+        System.out.println("Barbie Kitchen Set is a role-play toy");
+
+        String color = "Pink";
+        String brand = "Barbie";
+        byte pieces = 15;
+        int stock = 50;
+        short weight = 700;
+        long productNo = 55555555514L;
+        float price = 1499.50f;
+        String type = "Kitchen Set";
+        char grade = 'A';
+        boolean rolePlay = true;
+
+        System.out.println("color=" + color);
+        System.out.println("brand=" + brand);
+        System.out.println("pieces=" + pieces);
+        System.out.println("stock=" + stock);
+        System.out.println("weight=" + weight);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("type=" + type);
+        System.out.println("grade=" + grade);
+        System.out.println("rolePlay=" + rolePlay);
+    }
+}
