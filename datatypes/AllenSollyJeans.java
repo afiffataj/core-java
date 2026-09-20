99521536113437769ac67cb2c546dac8aeb7e791
afiffataj <afiffataj@gmail.com>
2026-09-20 23:37:31 +0530

    variables

---
+++ datatypes/AllenSollyJeans.java
@@ -0,0 +1,68 @@
+class AllenSollyJeans{
+	public static void main(String[] args){
+		System.out.println("AllenSollyJeans are comfortable");
+		
+		String materialType="Cotton";
+		String occasionType="Casual";
+		byte size=46;
+		int price=2499;
+		short discount=65;
+		float weight=399f;
+		double quantity=1d;
+		String color="Dark Blue";
+		long productNo=234566876l;
+		
+		System.out.println("materialType before re-initalization="+materialType);
+		System.out.println("occasionType before re-initalization="+occasionType);
+		System.out.println("size before re-initalization="+size);
+		System.out.println("price before re-initalization="+price);
+		System.out.println("discount before re-initalization="+discount);
+		System.out.println("weight before re-initalization="+weight);
+		System.out.println("quantity before re-initalization="+quantity);
+		System.out.println("color before re-initalization="+color);
+		System.out.println("productNo before re-initalization="+productNo);
+		System.out.println();
+		
+		materialType="Nylon";
+		occasionType="School Wear";
+		size=34;
+		price=3000;
+		discount=55;
+		weight=487f;
+		quantity=5d;
+		color="Red";
+		productNo=12567893736l;
+		
+		System.out.println("materialType after re-initalization="+materialType);
+		System.out.println("occasionType after re-initalization="+occasionType);
+		System.out.println("size after re-initalization="+size);
+		System.out.println("price after re-initalization="+price);
+		System.out.println("discount after re-initalization="+discount);
+		System.out.println("weight after re-initalization="+weight);
+		System.out.println("quantity after re-initalization="+quantity);
+		System.out.println("color after re-initalization="+color);
+		System.out.println("productNo after re-initalization="+productNo);
+		System.out.println();
+		
+		materialType="Silk";
+		occasionType="Collage Wear";
+		size=40;
+		price=2500;
+		discount=20;
+		weight=300f;
+		quantity=7d;
+		color="Black";
+		productNo=825687563787l;
+		
+		System.out.println("materialType after re-initalization="+materialType);
+		System.out.println("occasionType after re-initalization="+occasionType);
+		System.out.println("size after re-initalization="+size);
+		System.out.println("price after re-initalization="+price);
+		System.out.println("discount after re-initalization="+discount);
+		System.out.println("weight after re-initalization="+weight);
+		System.out.println("quantity after re-initalization="+quantity);
+		System.out.println("color after re-initalization="+color);
+		System.out.println("productNo after re-initalization="+productNo);
+		
+	}
+}
