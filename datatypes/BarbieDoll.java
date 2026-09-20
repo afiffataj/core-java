6dfb521d325f106ae7c1505018d628e7d5c96c65
afiffataj <afiffataj@gmail.com>
2026-09-20 23:33:43 +0530

    variables

---
+++ datatypes/BarbieDoll.java
@@ -0,0 +1,27 @@
+class BarbieDoll {
+    public static void main(String[] args) {
+        System.out.println("Barbie Doll is a fashion doll");
+
+        String color = "Pink";
+        String brand = "Barbie";
+        byte age = 3;
+        int stock = 60;
+        short height = 30;
+        long productNo = 55555555503L;
+        float price = 999.50f;
+        String type = "Fashion Doll";
+        char grade = 'A';
+        boolean movable = true;
+
+        System.out.println("color=" + color);
+        System.out.println("brand=" + brand);
+        System.out.println("age=" + age);
+        System.out.println("stock=" + stock);
+        System.out.println("height=" + height);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("type=" + type);
+        System.out.println("grade=" + grade);
+        System.out.println("movable=" + movable);
+    }
+}
