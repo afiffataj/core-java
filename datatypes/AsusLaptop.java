000edc0afd08def418ef539ec03936e8004ea2b4
afiffataj <afiffataj@gmail.com>
2026-09-20 23:35:07 +0530

    variables

---
+++ datatypes/AsusLaptop.java
@@ -0,0 +1,27 @@
+class AsusLaptop {
+    public static void main(String[] args) {
+        System.out.println("Asus Laptop is a computer");
+
+        String color = "Black";
+        String brand = "Asus";
+        byte usbPorts = 4;
+        int storage = 1024;
+        short weight = 1800;
+        long productNo = 12345678904L;
+        float price = 72999.50f;
+        String model = "Asus Vivobook";
+        char grade = 'A';
+        boolean gaming = true;
+
+        System.out.println("color=" + color);
+        System.out.println("brand=" + brand);
+        System.out.println("usbPorts=" + usbPorts);
+        System.out.println("storage=" + storage);
+        System.out.println("weight=" + weight);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("model=" + model);
+        System.out.println("grade=" + grade);
+        System.out.println("gaming=" + gaming);
+    }
+}
