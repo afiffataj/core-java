739ac25c47e6c1830e1494872359de4e4f48d745
afiffataj <afiffataj@gmail.com>
2026-09-20 23:31:04 +0530

    variables

---
+++ datatypes/BoatAirdopes.java
@@ -0,0 +1,27 @@
+class BoatAirdopes {
+    public static void main(String[] args) {
+        System.out.println("Boat Airdopes are wireless earbuds");
+
+        String color = "Black";
+        String brand = "Boat";
+        byte speakers = 2;
+        int battery = 40;
+        short weight = 45;
+        long productNo = 22345678901L;
+        float price = 1499.50f;
+        String model = "Airdopes 141";
+        char grade = 'A';
+        boolean bluetooth = true;
+
+        System.out.println("color=" + color);
+        System.out.println("brand=" + brand);
+        System.out.println("speakers=" + speakers);
+        System.out.println("battery=" + battery);
+        System.out.println("weight=" + weight);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("model=" + model);
+        System.out.println("grade=" + grade);
+        System.out.println("bluetooth=" + bluetooth);
+    }
+}
