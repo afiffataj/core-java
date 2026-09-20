8d1c4d581167587578f2907c1435c377030e4746
afiffataj <afiffataj@gmail.com>
2026-09-20 23:30:10 +0530

    variables

---
+++ datatypes/AdidasTrackPants.java
@@ -0,0 +1,27 @@
+class AdidasTrackPants {
+    public static void main(String[] args) {
+        System.out.println("Adidas Track Pants are sportswear");
+
+        String color = "Black";
+        String brand = "Adidas";
+        byte size = 32;
+        int stock = 90;
+        short length = 40;
+        long productNo = 33333333304L;
+        float price = 2499.50f;
+        String material = "Polyester";
+        char grade = 'A';
+        boolean stretchable = true;
+
+        System.out.println("color=" + color);
+        System.out.println("brand=" + brand);
+        System.out.println("size=" + size);
+        System.out.println("stock=" + stock);
+        System.out.println("length=" + length);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("material=" + material);
+        System.out.println("grade=" + grade);
+        System.out.println("stretchable=" + stretchable);
+    }
+}
