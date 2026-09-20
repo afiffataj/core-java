a36f33b2bec9f2d77f041a8963a524ed30e9741d
afiffataj <afiffataj@gmail.com>
2026-09-20 23:36:05 +0530

    variables

---
+++ datatypes/AppleWatch.java
@@ -0,0 +1,27 @@
+class AppleWatch {
+    public static void main(String[] args) {
+        System.out.println("Apple Watch is a smartwatch");
+
+        String color = "Black";
+        String brand = "Apple";
+        byte buttons = 2;
+        int battery = 18;
+        short weight = 45;
+        long productNo = 32345678901L;
+        float price = 45999.50f;
+        String model = "Watch Series";
+        char grade = 'A';
+        boolean healthTracking = true;
+
+        System.out.println("color=" + color);
+        System.out.println("brand=" + brand);
+        System.out.println("buttons=" + buttons);
+        System.out.println("battery=" + battery);
+        System.out.println("weight=" + weight);
+        System.out.println("productNo=" + productNo);
+        System.out.println("price=" + price);
+        System.out.println("model=" + model);
+        System.out.println("grade=" + grade);
+        System.out.println("healthTracking=" + healthTracking);
+    }
+}
