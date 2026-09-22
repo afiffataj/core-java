56df413fef4ae432d0e35dff26c1ab5a0603598b
afiffataj <afiffataj@gmail.com>
2026-09-22 16:37:12 +0530

    variables

---
+++ datatypes/BlackBoard.java
@@ -0,0 +1,29 @@
+class BlackBoard{
+	public static void main(String[] args){
+		System.out.println("BlackBoard is used to write");
+		
+		String color="Black";
+		int price=900;
+		byte quantity=5;
+		short noOfPiece=3;
+		long pincode=560064;
+		boolean isAvailable=true;
+		float weight=90.7f;
+		double length=78d;
+		char size='M';
+		String info="the file name is stored in meta space,the local variables are stored in stack memory and the non primitive like string is stored in string pole and all the primitive are stored in constant pole the variable names cant be repeated in primitive and constant polethe variable can be repeated in non primitive and string polewhen we execute code the jvm takes it line by line after execution of source code jvm starts taking the code one after another";
+		
+		System.out.println("color="+color);
+		System.out.println("price="+price);
+		System.out.println("quantity="+quantity);
+		System.out.println("noOfPiece="+noOfPiece);
+		System.out.println("pincode="+pincode);
+		System.out.println("isAvailable="+isAvailable);
+		System.out.println("weight="+weight);
+		System.out.println("length="+length);
+		System.out.println("size="+size);
+		System.out.println("info="+info);
+	}
+}
+
+ 
