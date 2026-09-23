c28b39d1285d54e9ab5a7172a2a1ed623551950a
afiffataj <afiffataj@gmail.com>
2026-09-23 16:51:47 +0530

    variables

---
+++ datatypes/Pizza.java
@@ -0,0 +1,22 @@
+class Pizza{
+	static String base="fresh";
+	static String topings="compulsory";
+    static int sizeInInch=35;	
+	
+	public static void main(String[] args){
+		System.out.println("Pizza is good for health");
+		
+		int price=900;
+		byte quantity=5;
+		String brand="Dominos";
+		char size='L';
+		
+		System.out.println(base);
+		System.out.println(topings);
+		System.out.println(sizeInInch);
+		System.out.println(price);
+		System.out.println(quantity);
+		System.out.println(brand);
+		System.out.println(size);
+	}
+}
