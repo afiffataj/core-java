1c2db983127c65fff1d76632bff551a7814a62df
afiffataj <afiffataj@gmail.com>
2026-09-23 23:39:18 +0530

    static

---
+++ Static Variables/MedicalForm.java
@@ -0,0 +1,72 @@
+class MedicalForm{
+	static String medicalName="Apolo";
+	static String medicalLocation="Kengeri";
+	static long phoneNoOfOwner=9809643678l;
+	static boolean avaiable=true;
+	static String timming="24/7";
+
+	public static void main(String[] args){
+        System.out.println("MedicalForm is important");
+		
+		String medicineName;
+		int quantity;
+		String manufactureDate;
+		String brand;
+		byte price;
+		
+		System.out.println("1st Medicine");
+		medicineName="Paracetamol";
+		quantity=4;
+		manufactureDate="feb-8";
+		brand="Hamdard";
+		price=110;
+		System.out.println("medicalName="+MedicalForm.medicalName);
+		System.out.println("medicalLocation="+MedicalForm.medicalLocation);
+		System.out.println("phoneNoOfOwner="+MedicalForm.phoneNoOfOwner);
+		System.out.println("avaiable="+MedicalForm.avaiable);
+		System.out.println("timming="+MedicalForm.timming);
+		System.out.println("medicineName="+medicineName);
+		System.out.println("quantity="+quantity);
+		System.out.println("manufactureDate="+manufactureDate);
+		System.out.println("brand="+brand);
+		System.out.println("price="+price);
+		System.out.println();
+		
+		System.out.println("2nd Medicine");
+		medicineName="Amoxicillian";
+		quantity=7;
+		manufactureDate="march-18";
+		brand="dabur";
+		price=100;
+		System.out.println("medicalName="+MedicalForm.medicalName);
+		System.out.println("medicalLocation="+MedicalForm.medicalLocation);
+		System.out.println("phoneNoOfOwner="+MedicalForm.phoneNoOfOwner);
+		System.out.println("avaiable="+MedicalForm.avaiable);
+		System.out.println("timming="+MedicalForm.timming);
+		System.out.println("medicineName="+medicineName);
+		System.out.println("quantity="+quantity);
+		System.out.println("manufactureDate="+manufactureDate);
+		System.out.println("brand="+brand);
+		System.out.println("price="+price);
+	    System.out.println();
+	  
+		System.out.println("3rd Medicine");
+		medicineName="Dolo";
+		quantity=2;
+		manufactureDate="sept-3";
+		brand="Safi";
+		price=90;
+		System.out.println("medicalName="+MedicalForm.medicalName);
+		System.out.println("medicalLocation="+MedicalForm.medicalLocation);
+		System.out.println("phoneNoOfOwner="+MedicalForm.phoneNoOfOwner);
+		System.out.println("avaiable="+MedicalForm.avaiable);
+		System.out.println("timming="+MedicalForm.timming);
+		System.out.println("medicineName="+medicineName);
+		System.out.println("quantity="+quantity);
+		System.out.println("manufactureDate="+manufactureDate);
+		System.out.println("brand="+brand);
+		System.out.println("price="+price);
+		
+	}
+}
+        		
