1ffbd11b53b517ffe948b3d2c7bef0119cc8b784
afiffataj <afiffataj@gmail.com>
2026-09-23 16:48:30 +0530

    variables

---
+++ datatypes/Television.java
@@ -0,0 +1,23 @@
+class Television{
+	static String color="Black";
+	static int quantity=1;
+	static char size='M';
+	
+	public static void main(String[] args){
+		System.out.println("Television are used in 90's");
+		
+		int price=9000;
+		String brand="usha";
+		double length=50.6;
+		float weight=30;
+		
+		System.out.println(color);
+		System.out.println(quantity);
+		System.out.println(size);
+		System.out.println(price);
+		System.out.println(brand);
+		System.out.println(length);
+		System.out.println(weight);
+	}
+}
+		
