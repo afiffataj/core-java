b43e83fabc08e51a0b12daf0a1353a22069fb45f
afiffataj <afiffataj@gmail.com>
2026-09-23 16:49:52 +0530

    variables

---
+++ datatypes/SwitchBoard.java
@@ -0,0 +1,20 @@
+class SwitchBoard{
+	static String brand="wiff";
+	static boolean isAvailable=true;
+	static boolean safe=false;
+	
+	public static void main(String[] args){
+		System.out.println("SwitchBoard is dangerous");
+		
+		int price=500;
+		byte noOfPlugs=9;
+		String color="White";
+		
+		System.out.println(brand);
+		System.out.println(isAvailable);
+		System.out.println(safe);
+		System.out.println(price);
+		System.out.println(noOfPlugs);
+		System.out.println(color);
+	}
+}
