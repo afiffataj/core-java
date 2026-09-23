245265d348998b418a35fbbb01e170de4205454a
afiffataj <afiffataj@gmail.com>
2026-09-23 16:50:51 +0530

    variables

---
+++ datatypes/CollegeBag.java
@@ -0,0 +1,30 @@
+class CollegeBag{
+	static int pockets=1;
+	static byte noOfZips=4;
+	static int bottleHolder=1;
+	static int bagHolder=1;
+	static String brand="Skybags";
+	
+	public static void main(String[] args){
+		System.out.println("CollegeBag are used for carrying book");
+		
+		int price=1090;
+		String color="Blue";
+		boolean isAvailable=true;
+		String type="long";
+		char size='L';
+		
+		System.out.println(pockets);
+	    System.out.println(noOfZips);
+	    System.out.println(bottleHolder);
+	    System.out.println(bagHolder);
+	    System.out.println(brand);
+		System.out.println(price);
+		System.out.println(color);
+		System.out.println(isAvailable);
+		System.out.println(type);
+		System.out.println(size);
+	}
+}
+		
+		
