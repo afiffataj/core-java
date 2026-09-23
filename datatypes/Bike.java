f1d1f287952bd58cc10383a8c10045c556a11587
afiffataj <afiffataj@gmail.com>
2026-09-23 16:45:44 +0530

    variables

---
+++ datatypes/Bike.java
@@ -0,0 +1,22 @@
+class Bike{
+	static int quantity=1;
+	static double weight=578.89d;
+	static short capacity=588;
+	
+	public static void main(String[] args){
+		System.out.println("Bikes are dangerous");
+		
+		String brand="royal enfield";
+		int price=200000;
+		String color="Bllack";
+		double length=690.7d;
+		
+		System.out.println(quantity);
+		System.out.println(weight);
+		System.out.println(capacity);
+		System.out.println(brand);
+        System.out.println(price);
+        System.out.println(color);		
+		System.out.println(length);
+	}
+}
