daff88c420a7241b70787fec3e7ff4ebade1e422
afiffataj <afiffataj@gmail.com>
2026-09-24 23:30:27 +0530

    static

---
+++ Static Variables/StreeLight.java
@@ -0,0 +1,34 @@
+class StreeLight{
+	static String color;
+	static int lengthOfRoad;
+	static short weightInCm;
+	static long pincode;
+	static char size;
+	static float width;
+	static boolean isWorking;
+	static double lightCapacity;
+
+	public static void main(String[] args){
+		System.out.println("StreeLight is used for roads");
+		
+		int price=5687;
+		String brand="Raju";
+		byte quantity=4;
+		char length='L';
+		
+		System.out.println("color="+StreeLight.color);
+		System.out.println("lengthOfRoad="+StreeLight.lengthOfRoad);
+		System.out.println("weightInCm="+StreeLight.weightInCm);
+		System.out.println("pincode="+StreeLight.pincode);
+		System.out.println("size="+StreeLight.size);
+		System.out.println("width="+StreeLight.width);
+		System.out.println("isWorking="+StreeLight.isWorking);
+		System.out.println("lightCapacity="+StreeLight.lightCapacity);
+		System.out.println(price);
+		System.out.println(brand);
+		System.out.println(quantity);
+		System.out.println(length);
+	}
+}
+		
+		
