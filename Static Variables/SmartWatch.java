c095fba7c511c48c2612e4171b303132d459330e
afiffataj <afiffataj@gmail.com>
2026-09-25 23:10:29 +0530

    static

---
+++ Static Variables/SmartWatch.java
@@ -0,0 +1,30 @@
+class SmartWatch{
+	static String type;
+	static byte quantity;
+	static int length;
+	static long productName;
+	static float weight;
+	static boolean isAvailable;
+	static double capacity;
+	static char size;
+	
+	public static void main(String[] args){
+		System.out.println("SmartWatch is used as AI technology");
+	
+        String brand="Apple";
+		int price=4567;
+		byte discount=45;
+		
+		System.out.println(SmartWatch.type);
+		System.out.println(SmartWatch.quantity);
+		System.out.println(SmartWatch.length);
+		System.out.println(SmartWatch.productName);
+		System.out.println(SmartWatch.weight);
+		System.out.println(SmartWatch.isAvailable);
+		System.out.println(SmartWatch.capacity);
+		System.out.println(SmartWatch.size);
+		System.out.println(brand);
+		System.out.println(price);
+		System.out.println(discount);
+	}
+}
