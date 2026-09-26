26d42463bdb64d9a3b2a7b9531487eb64cf96326
afiffataj <afiffataj@gmail.com>
2026-09-26 23:11:14 +0530

    static

---
+++ Static Variables/Adapter.java
@@ -0,0 +1,25 @@
+class Adapter{
+	static int inputVoltage=120;
+	static byte noOfITem=2;
+	static String color="Snow White";
+	static String connectorType="USB Type c";
+	
+	public static void main(String[] args){
+		System.out.println("Adapter is charger holder");
+		
+		int price=299;
+		byte discount=80;
+		String brand="Kratos";
+		
+		System.out.println(Adapter.inputVoltage);
+		System.out.println(Adapter.noOfITem);
+		System.out.println(Adapter.color);
+		System.out.println(Adapter.connectorType);
+		System.out.println(price);
+		System.out.println(discount);
+		System.out.println(brand);
+	}
+}
+
+		
+		
