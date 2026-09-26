84b1abfa2fec5b534cb17172e9c2dcc56af21572
afiffataj <afiffataj@gmail.com>
2026-09-26 21:48:42 +0530

    static

---
+++ Static Variables/WaterBottle.java
@@ -0,0 +1,32 @@
+class WaterBottle{
+	static String country;
+    static int capacity;
+	static String shape;
+	static String styleName;
+	
+	public static void main(String[] args){
+		System.out.println("WaterBottle is easy-to-clean");
+		
+		String brand="Lifelong";
+		String model="WaterBottles";
+		int weight=195;
+		byte quantity=5;
+		
+		WaterBottle.country="India";
+	WaterBottle.capacity=900;
+	WaterBottle.shape="Round";
+	WaterBottle.styleName="Modern";
+		
+		System.out.println(WaterBottle.country);
+		System.out.println(WaterBottle.capacity);
+		System.out.println(WaterBottle.shape);
+		System.out.println(WaterBottle.styleName);
+		System.out.println(brand);
+		System.out.println(model);
+		System.out.println(weight);
+		System.out.println(quantity);
+	}
+}
+
+		
+		
